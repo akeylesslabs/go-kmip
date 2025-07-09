8f350e091ce83f89ed0006e00f850851cbac4fa2
tuvia-akeyless <tuvia.r@akeyless.io>
2025-07-09 17:45:19 +0300

    without granting_private_modules_access

--- .github/scripts/common/granting_private_modules_access.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-set -e  # exit on error
-
-if [[ -n ${GITHUB_TOKEN} ]]; then
-  git config --global url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/akeylesslabs".insteadOf "https://github.com/akeylesslabs"
-fi
