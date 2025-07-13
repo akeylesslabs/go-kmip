704a80539dc04b9959436d29d15d2c7a38374bfb
tuvia-akeyless <tuvia.r@akeyless.io>
2025-07-13 08:48:30 +0300

    from dir

--- .github/scripts/run_go_tests.sh
+++ .github/scripts/run_go_tests.sh
@@ -48,8 +48,6 @@ all_tests_list=("kmip")
 
 function test_kmip() {
   echo "Testing KMIP package"
-  pwd
-  cd ./work/go-kmip/go-kmip
   go test -v -race -timeout 300s ./... ${go_tags:+"-tags" "$go_tags"}
 }
 
